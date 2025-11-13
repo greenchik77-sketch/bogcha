a6e4db6f37b2e13848ac4b07fd8e5b3acbc21eeb
greenchik77-sketch <greenchik77@gmail.com>
2025-11-13 10:26:27 +0500

    bazani mysqldan sqllite ga ozgartirdim

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
+from database import Base, engine
 from routers.kindergartens import kindergarten_router
 from routers.users import user_router
 
 app = FastAPI(title="Bog'cha", docs_url="/")
 
+Base.metadata.create_all(engine)
 
 app.include_router(user_router, tags=["Users"])
 
